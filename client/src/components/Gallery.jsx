70a48fb8e09e820fb5d8536a89fb3dfbbfc1d6bd
iSaklov <ivansaklov@gmail.com>
2023-03-23 04:50:20 +0100

    feat: add navigation to section

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Gallery = () => {
   return (
     <section>
-      <h1>GALLERY</h1>
+      <h1 id='gallery'>GALLERY</h1>
     </section>
   )
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,71 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
+import React, { useState } from 'react'
+import { Container, Nav, Navbar, Offcanvas } from 'react-bootstrap'
+import Logo from '../assets/logo.svg'
 
 const NavBar = () => {
+  const [show, setShow] = useState(false)
+
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar expand="md" variant="dark" sticky="top">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <Navbar.Brand href="#header">
+          <img
+            src={Logo}
+            width="200" //TODO move to Style
+            height="88.5"
+            className="align-top d-none d-md-inline-block "
+            alt="H.M. - barber shop logo"
+          />
+        </Navbar.Brand>
+
+        <Navbar.Toggle
+          aria-controls="offcanvasNavbar-expand"
+          onClick={setShow.bind(null, !show)}
+        />
+
+        <Navbar.Offcanvas
+          id="offcanvasNavbar-expand"
+          aria-labelledby="offcanvasNavbarLabel-expand"
+          placement="end" // = rigth
+          show={show}
+          onHide={() => {
+            setShow(!show)
+          }}
+          //TODO opacity transition for logo
+          // onShow={test}
+          // onExiting={test}
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id="offcanvasNavbarLabel-expand">
+              <Nav.Link href="#header" onClick={setShow.bind(null, !show)}>
+                <img
+                  src={Logo}
+                  width="200" //TODO move to Style
+                  height="88.5"
+                  className="align-top" //? default Navbar example
+                  alt="H.M. - barber shop logo"
+                />
+              </Nav.Link>
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            {/* <Nav className="justify-content-end flex-grow-1 pe-3"> */}
+            <Nav className="">
+              <Nav.Link href="#price-list" onClick={setShow.bind(null, !show)}>
+                Nos tarifs
+              </Nav.Link>
+              <Nav.Link href="#gallery" onClick={setShow.bind(null, !show)}>
+                Gallérie
+              </Nav.Link>
+              <Nav.Link href="#about" onClick={setShow.bind(null, !show)}>
+                À propos
+              </Nav.Link>
+              <Nav.Link href="#footer" onClick={setShow.bind(null, !show)}>
+                Contacts
+              </Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
       </Container>
     </Navbar>
   )
