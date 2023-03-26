284c7bb3d1762543bd3e54ff068ef8d28587aceb
eugeniakhaleeva <khaleeva20990@gmail.com>
2023-03-26 22:44:38 +0200

    feat: add general section

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import { default as Navbar } from '../components/Navbar'
-import Header from '../components/Header'
+import {default as Navbar} from '../components/Navbar'
+
 import PriceList from '../components/PriceList'
 import Gallery from '../components/Gallery'
 import About from '../components/About'
 import Footer from '../components/Footer'
+import GeneralSection from "../components/GeneralSection";
 
 const HomePage = () => {
-  return (
-    <>
-      <Navbar />
-      <main>
-        <Header />
-        <PriceList />
-        <Gallery />
-        <About />
-      </main>
-      <Footer />
-    </>
-  )
+    return (
+        <div className='wrapper'>
+            <Navbar/>
+            <main className='main'>
+                <GeneralSection/>
+                <PriceList/>
+                <Gallery/>
+                <About/>
+            </main>
+            <Footer/>
+        </div>
+    )
 }
 
 export default HomePage
