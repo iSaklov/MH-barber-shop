db2bbb6936c5f58fdf9bdb1ea49c64f33df5750e
iSaklov <ivansaklov@gmail.com>
2023-05-10 01:51:07 +0200

    feat: structuring styles files by BEM

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,27 +1,26 @@
-import {default as Navbar} from '../components/Navbar'
-
+import { default as Navbar } from '../components/Navbar'
 import PriceList from '../components/PriceList'
 import Gallery from '../components/Gallery'
 import About from '../components/About'
 import Footer from '../components/Footer'
-import GeneralSection from "../components/GeneralSection";
-import ContainerSection from "../components/ContainerSection";
+import GeneralSection from '../components/GeneralSection'
+import ContainerSection from '../components/ContainerSection'
 
 const HomePage = () => {
-    return (
-        <div className='wrapper'>
-            <Navbar/>
-            <main className='main'>
-                <GeneralSection/>
-                <PriceList/>
-                <ContainerSection>
-                    <Gallery/>
-                    <About/>
-                </ContainerSection>
-            </main>
-            <Footer/>
-        </div>
-    )
+  return (
+    <div className="wrapper">
+      <Navbar />
+      <main className="main">
+        <GeneralSection />
+        <PriceList />
+        <ContainerSection>
+          <Gallery />
+          <About />
+        </ContainerSection>
+      </main>
+      <Footer />
+    </div>
+  )
 }
 
 export default HomePage
