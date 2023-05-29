b356a88bf88f41ba63ffbffc6c3e9a9c1fb4cd83
iSaklov <ivansaklov@gmail.com>
2023-05-29 17:11:27 +0200

    feat: navbar navigation of <Offcanvas>

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useCallback, useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Container, Nav, Navbar, Offcanvas } from 'react-bootstrap'
 import Logo from '../assets/logo.svg'
 
@@ -7,6 +8,7 @@ const NavBar = () => {
   const [navbarHidden, setNavbarHidden] = useState(false)
   const [lastScrollY, setLastScrollY] = useState(0)
   const sectionsRef = useRef([])
+  const navigate = useNavigate()
 
   const controlNavbar = useCallback(() => {
     if (window.scrollY > lastScrollY) {
@@ -14,14 +16,14 @@ const NavBar = () => {
     } else {
       setNavbarHidden(false)
     }
-    setLastScrollY(Math.abs(window.scrollY))
+    setLastScrollY(window.scrollY)
   }, [lastScrollY])
 
   const navHighlighter = useCallback(() => {
     const scrollY = window.pageYOffset
     sectionsRef.current.forEach((section) => {
       const sectionHeight = section.offsetHeight
-      const sectionTop = section.offsetTop - window.innerHeight * 0.75;
+      const sectionTop = section.offsetTop - window.innerHeight * 0.75
       const sectionId = section.getAttribute('id')
       const link = document.querySelector(`.navbar-nav a[href*="${sectionId}"]`)
       if (link) {
@@ -34,6 +36,36 @@ const NavBar = () => {
     })
   }, [])
 
+  const handleScroll = useCallback(() => {
+    controlNavbar()
+    navHighlighter()
+  }, [controlNavbar, navHighlighter])
+
+  const handleLinkClick = (event, sectionId = null) => {
+    if (event) {
+      // without canceling the standard behavior, navigation to the desired section from Offcanvas did not occur
+      event.preventDefault()
+    }
+
+    if (sectionId) {
+      const section = document.getElementById(sectionId)
+      window.scrollTo({
+        top: section.offsetTop,
+        behavior: 'smooth'
+      })
+      navigate(`#${sectionId}`)
+    }
+
+    if (window.matchMedia('(max-width: 767.98px)').matches) {
+      // avoids showOffcanvas state change on non-mobile devices
+      setShowOffcanvas(!showOffcanvas)
+    }
+  }
+
+  // useEffect(() => {
+  //   console.log('showOffcanvas', showOffcanvas)
+  // }, [showOffcanvas])
+
   useEffect(() => {
     sectionsRef.current = document.querySelectorAll('section[id], footer[id]')
   }, [])
@@ -42,21 +74,11 @@ const NavBar = () => {
     if (typeof window === 'undefined') {
       return
     }
-    window.addEventListener('scroll', controlNavbar)
+    window.addEventListener('scroll', handleScroll)
     return () => {
-      window.removeEventListener('scroll', controlNavbar)
+      window.removeEventListener('scroll', handleScroll)
     }
-  }, [controlNavbar])
-
-  useEffect(() => {
-    if (typeof window === 'undefined') {
-      return
-    }
-    window.addEventListener('scroll', navHighlighter)
-    return () => {
-      window.removeEventListener('scroll', navHighlighter)
-    }
-  }, [navHighlighter])
+  }, [handleScroll])
 
   return (
     <Navbar
@@ -73,55 +95,55 @@ const NavBar = () => {
             alt="H.M. - barber shop logo"
           />
         </Navbar.Brand>
-
         <Navbar.Toggle
           aria-controls="offcanvasNavbar-expand"
-          onClick={setShowOffcanvas.bind(null, !showOffcanvas)}
+          onClick={(event) => handleLinkClick(event, null)}
         />
-
         <Navbar.Offcanvas
           id="offcanvasNavbar-expand"
           aria-labelledby="offcanvasNavbarLabel-expand"
           placement="end" // rigth
           show={showOffcanvas}
-          onHide={() => {
-            setShowOffcanvas(!showOffcanvas)
-          }}
+          onHide={handleLinkClick.bind(null)}
+          scroll={false}
+          backdrop={true}
           responsive="md"
         >
           <Offcanvas.Header closeButton closeVariant="white">
             <Offcanvas.Title id="offcanvasNavbarLabel-expand">
               <Nav.Link
                 href="#hero-section"
-                onClick={setShowOffcanvas.bind(null, !showOffcanvas)}
+                onClick={(event) => handleLinkClick(event, 'hero-section')}
               >
                 <img src={Logo} alt="H.M. - barber shop logo" />
               </Nav.Link>
             </Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
-            <Nav>
+            <Nav activeKey={null}>
               <Nav.Link
                 href="#price-list-section"
-                onClick={setShowOffcanvas.bind(null, !showOffcanvas)}
+                onClick={(event) =>
+                  handleLinkClick(event, 'price-list-section')
+                }
               >
                 Nos tarifs
               </Nav.Link>
               <Nav.Link
                 href="#gallery-section"
-                onClick={setShowOffcanvas.bind(null, !showOffcanvas)}
+                onClick={(event) => handleLinkClick(event, 'gallery-section')}
               >
                 Gallérie
               </Nav.Link>
               <Nav.Link
                 href="#about-us-section"
-                onClick={setShowOffcanvas.bind(null, !showOffcanvas)}
+                onClick={(event) => handleLinkClick(event, 'about-us-section')}
               >
                 À propos
               </Nav.Link>
               <Nav.Link
                 href="#footer"
-                onClick={setShowOffcanvas.bind(null, !showOffcanvas)}
+                onClick={(event) => handleLinkClick(event, 'footer')}
               >
                 Contacts
               </Nav.Link>
