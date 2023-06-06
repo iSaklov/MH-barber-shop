60b593da9a36839c10ee4bce12fa392783b1e25a
iSaklov <ivansaklov@gmail.com>
2023-06-06 17:08:45 +0200

    feat: change Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,12 @@
-
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import useRoutes from './routes'
 
 function App() {
   const isAdmin = false
-	const routes = useRoutes(isAdmin)
+  const routes = useRoutes(isAdmin)
 
-  return (
-    <BrowserRouter>
-      {routes}
-    </BrowserRouter>
-  )
+  return <HashRouter>{routes}</HashRouter>
 }
 
 export default App
